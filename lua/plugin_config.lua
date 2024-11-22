ce50bfa7f5eea16b97591d69afb430fcb266ef61
Lev Poliakov <progsag@gmail.com>
2024-11-22 19:51:41 +0700

    Configuration clean-up

---
+++ lua/plugin_config.lua
@@ -0,0 +1,429 @@
+local lualine = require('lualine')
+
+lualine.setup{
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  extensions = { 'fzf' },
+  sections = {
+    lualine_a = {{'mode', format=function(mode_name) return mode_name:sub(1,1) end}, },
+    lualine_b = {{'branch'}},
+    lualine_c = {
+      {'filename', condition=function() return vim.fn.winwidth(0) > 50 end},
+      {'diagnostics', sources={'nvim_diagnostic'}, condition=function() return vim.fn.winwidth(0) > 80 end}
+    },
+    lualine_x = {{'encoding', 'fileformat', 'filetype'}},
+    lualine_y = {{'diff'}},
+    lualine_z = {{'location'}}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+      {'filename', condition=function() return vim.fn.winwidth(0) > 50 end},
+      {'diagnostics', sources={'nvim_diagnostic'}, condition=function() return vim.fn.winwidth(0) > 80 end}
+    },
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+}
+
+Lspconfig = require('lspconfig')
+
+Lspfuzzy = require('lspfuzzy')
+Lspfuzzy.setup {}
+
+Treesitter = require('nvim-treesitter.configs')
+Treesitter.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "python", "cpp", "lua" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+-- Diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+	-- Enable underline, use default values
+	underline = true,
+  	virtual_text = true,
+        signs = true,
+  	update_in_insert = false
+  }
+)
+
+Trouble = require('trouble')
+Trouble.setup {
+  signs = {
+    -- icons / text used for a diagnostic
+    error = "",
+    warning = "",
+    hint = "",
+    information = "",
+    other = "﫠"
+  },
+}
+
+--- Unfiltered mess below
+
+local function preview_location_callback(_, _, result)
+  if result == nil or vim.tbl_isempty(result) then
+    return nil
+  end
+  vim.lsp.util.preview_location(result[1])
+end
+
+function RestartLSP()
+  local clients = vim.lsp.get_active_clients() -- client that crashed
+  local copy_of_config = clients[1].config
+  vim.lsp.stop_client(clients)
+  local new_client_id = vim.lsp.start_client(copy_of_config)
+  vim.lsp.buf_attach_client(0, new_client_id)
+end
+
+function PeekDefinition()
+  local params = vim.lsp.util.make_position_params()
+  return vim.lsp.buf_request(0, 'textDocument/definition', params, preview_location_callback)
+end
+
+
+
+require("dressing").setup({
+  input = {
+    -- Set to false to disable the vim.ui.input implementation
+    enabled = true,
+
+    -- Default prompt string
+    default_prompt = "Input",
+
+    -- Trim trailing `:` from prompt
+    trim_prompt = true,
+
+    -- Can be 'left', 'right', or 'center'
+    title_pos = "left",
+
+    -- When true, <Esc> will close the modal
+    insert_only = true,
+
+    -- When true, input will start in insert mode.
+    start_in_insert = true,
+
+    -- These are passed to nvim_open_win
+    border = "rounded",
+    -- 'editor' and 'win' will default to being centered
+    relative = "cursor",
+
+    -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+    prefer_width = 40,
+    width = nil,
+    -- min_width and max_width can be a list of mixed types.
+    -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+    max_width = { 140, 0.9 },
+    min_width = { 20, 0.2 },
+
+    buf_options = {},
+    win_options = {
+      -- Disable line wrapping
+      wrap = false,
+      -- Indicator for when text exceeds window
+      list = true,
+      listchars = "precedes:…,extends:…",
+      -- Increase this for more context when text scrolls off the window
+      sidescrolloff = 0,
+    },
+
+    -- Set to `false` to disable
+    mappings = {
+      n = {
+        ["<Esc>"] = "Close",
+        ["<CR>"] = "Confirm",
+      },
+      i = {
+        ["<C-c>"] = "Close",
+        ["<CR>"] = "Confirm",
+        ["<Up>"] = "HistoryPrev",
+        ["<Down>"] = "HistoryNext",
+      },
+    },
+
+    override = function(conf)
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      return conf
+    end,
+
+    -- see :help dressing_get_config
+    get_config = nil,
+  },
+  select = {
+    -- Set to false to disable the vim.ui.select implementation
+    enabled = true,
+
+    -- Priority list of preferred vim.select implementations
+    backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
+
+    -- Trim trailing `:` from prompt
+    trim_prompt = true,
+
+    -- Options for telescope selector
+    -- These are passed into the telescope picker directly. Can be used like:
+    -- telescope = require('telescope.themes').get_ivy({...})
+    telescope = nil,
+
+    -- Options for fzf selector
+    fzf = {
+      window = {
+        width = 0.5,
+        height = 0.4,
+      },
+    },
+
+    -- Options for fzf-lua
+    fzf_lua = {
+      -- winopts = {
+      --   height = 0.5,
+      --   width = 0.5,
+      -- },
+    },
+
+    -- Options for nui Menu
+    nui = {
+      position = "50%",
+      size = nil,
+      relative = "editor",
+      border = {
+        style = "rounded",
+      },
+      buf_options = {
+        swapfile = false,
+        filetype = "DressingSelect",
+      },
+      win_options = {
+        winblend = 0,
+      },
+      max_width = 80,
+      max_height = 40,
+      min_width = 40,
+      min_height = 10,
+    },
+
+    -- Options for built-in selector
+    builtin = {
+      -- Display numbers for options and set up keymaps
+      show_numbers = true,
+      -- These are passed to nvim_open_win
+      border = "rounded",
+      -- 'editor' and 'win' will default to being centered
+      relative = "editor",
+
+      buf_options = {},
+      win_options = {
+        cursorline = true,
+        cursorlineopt = "both",
+      },
+
+      -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      -- the min_ and max_ options can be a list of mixed types.
+      -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
+      width = nil,
+      max_width = { 140, 0.8 },
+      min_width = { 40, 0.2 },
+      height = nil,
+      max_height = 0.9,
+      min_height = { 10, 0.2 },
+
+      -- Set to `false` to disable
+      mappings = {
+        ["<Esc>"] = "Close",
+        ["<C-c>"] = "Close",
+        ["<CR>"] = "Confirm",
+      },
+
+      override = function(conf)
+        -- This is the config that will be passed to nvim_open_win.
+        -- Change values here to customize the layout
+        return conf
+      end,
+    },
+
+    -- Used to override format_item. See :help dressing-format
+    format_item_override = {},
+
+    -- see :help dressing_get_config
+    get_config = nil,
+  },
+})
+
+
+require("cmake-tools").setup {
+  cmake_command = "cmake", -- this is used to specify cmake command path
+  ctest_command = "ctest", -- this is used to specify ctest command path
+  cmake_regenerate_on_save = true, -- auto generate when save CMakeLists.txt
+  cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
+  cmake_build_options = {}, -- this will be passed when invoke `CMakeBuild`
+  -- support macro expansion:
+  --       ${kit}
+  --       ${kitGenerator}
+  --       ${variant:xx}
+  cmake_build_directory = "out/${variant:buildType}", -- this is used to specify generate directory for cmake, allows macro expansion, relative to vim.loop.cwd()
+  cmake_soft_link_compile_commands = true, -- this will automatically make a soft link from compile commands file to project root dir
+  cmake_compile_commands_from_lsp = false, -- this will automatically set compile commands file location using lsp, to use it, please set `cmake_soft_link_compile_commands` to false
+  cmake_kits_path = nil, -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
+  cmake_variants_message = {
+    short = { show = true }, -- whether to show short message
+    long = { show = true, max_length = 40 }, -- whether to show long message
+  },
+  cmake_dap_configuration = { -- debug settings for cmake
+    name = "cpp",
+    type = "codelldb",
+    request = "launch",
+    stopOnEntry = false,
+    runInTerminal = true,
+    console = "integratedTerminal",
+  },
+  cmake_executor = { -- executor to use
+    name = "quickfix", -- name of the executor
+    opts = {}, -- the options the executor will get, possible values depend on the executor type. See `default_opts` for possible values.
+    default_opts = { -- a list of default and possible values for executors
+      quickfix = {
+        show = "always", -- "always", "only_on_error"
+        position = "belowright", -- "vertical", "horizontal", "leftabove", "aboveleft", "rightbelow", "belowright", "topleft", "botright", use `:h vertical` for example to see help on them
+        size = 10,
+        encoding = "utf-8", -- if encoding is not "utf-8", it will be converted to "utf-8" using `vim.fn.iconv`
+        auto_close_when_success = true, -- typically, you can use it with the "always" option; it will auto-close the quickfix buffer if the execution is successful.
+      },
+      toggleterm = {
+        direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float'
+        close_on_exit = false, -- whether close the terminal when exit
+        auto_scroll = true, -- whether auto scroll to the bottom
+      },
+      overseer = {
+        new_task_opts = {
+            strategy = {
+                "toggleterm",
+                direction = "horizontal",
+                autos_croll = true,
+                quit_on_exit = "success"
+            }
+        }, -- options to pass into the `overseer.new_task` command
+        on_new_task = function(task)
+            require("overseer").open(
+                { enter = false, direction = "right" }
+            )
+        end,   -- a function that gets overseer.Task when it is created, before calling `task:start`
+      },
+      terminal = {
+        name = "Main Terminal",
+        prefix_name = "[CMakeTools]: ", -- This must be included and must be unique, otherwise the terminals will not work. Do not use a simple spacebar " ", or any generic name
+        split_direction = "horizontal", -- "horizontal", "vertical"
+        split_size = 11,
+
+        -- Window handling
+        single_terminal_per_instance = true, -- Single viewport, multiple windows
+        single_terminal_per_tab = true, -- Single viewport per tab
+        keep_terminal_static_location = true, -- Static location of the viewport if avialable
+
+        -- Running Tasks
+        start_insert = false, -- If you want to enter terminal with :startinsert upon using :CMakeRun
+        focus = false, -- Focus on terminal when cmake task is launched.
+        do_not_add_newline = false, -- Do not hit enter on the command inserted when using :CMakeRun, allowing a chance to review or modify the command before hitting enter.
+      }, -- terminal executor uses the values in cmake_terminal
+    },
+  },
+  cmake_runner = { -- runner to use
+    name = "terminal", -- name of the runner
+    opts = {}, -- the options the runner will get, possible values depend on the runner type. See `default_opts` for possible values.
+    default_opts = { -- a list of default and possible values for runners
+      quickfix = {
+        show = "always", -- "always", "only_on_error"
+        position = "belowright", -- "bottom", "top"
+        size = 10,
+        encoding = "utf-8",
+        auto_close_when_success = true, -- typically, you can use it with the "always" option; it will auto-close the quickfix buffer if the execution is successful.
+      },
+      toggleterm = {
+        direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float'
+        close_on_exit = false, -- whether close the terminal when exit
+        auto_scroll = true, -- whether auto scroll to the bottom
+      },
+      overseer = {
+        new_task_opts = {
+            strategy = {
+                "toggleterm",
+                direction = "horizontal",
+                autos_croll = true,
+                quit_on_exit = "success"
+            }
+        }, -- options to pass into the `overseer.new_task` command
+        on_new_task = function(task)
+        end,   -- a function that gets overseer.Task when it is created, before calling `task:start`
+      },
+      terminal = {
+        name = "Main Terminal",
+        prefix_name = "[CMakeTools]: ", -- This must be included and must be unique, otherwise the terminals will not work. Do not use a simple spacebar " ", or any generic name
+        split_direction = "horizontal", -- "horizontal", "vertical"
+        split_size = 11,
+
+        -- Window handling
+        single_terminal_per_instance = true, -- Single viewport, multiple windows
+        single_terminal_per_tab = true, -- Single viewport per tab
+        keep_terminal_static_location = true, -- Static location of the viewport if avialable
+
+        -- Running Tasks
+        start_insert = false, -- If you want to enter terminal with :startinsert upon using :CMakeRun
+        focus = false, -- Focus on terminal when cmake task is launched.
+        do_not_add_newline = false, -- Do not hit enter on the command inserted when using :CMakeRun, allowing a chance to review or modify the command before hitting enter.
+      },
+    },
+  },
+  cmake_notifications = {
+    runner = { enabled = true },
+    executor = { enabled = true },
+    spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }, -- icons used for progress display
+    refresh_rate_ms = 100, -- how often to iterate icons
+  },
+}
+
+local neogit = require('neogit')
+neogit.setup {}
+
+require("hardtime").setup()
+-- Markdown setup
+local presets = require("markview.presets");
+
+require("markview").setup({
+    headings = presets.headings.arrowed,
+    checkboxes = presets.checkboxes.nerd
+});
